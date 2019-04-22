64d84aa5ddaa92634293fdab5bc853006d20826d
Andreas Hammer Håversen <37153174+AndreasHaaversen@users.noreply.github.com>
2019-04-22 03:59:12 +0200

    Added bubblesort and quicksort

--- src/assets/js/bubblesort.js
+++ src/assets/js/bubblesort.js
@@ -3,40 +3,63 @@ let p5;
 let values;
 
 let i = 0;
+let j = 0;
+let z = 0;
+
+let w = 20;
 
 export function main(_p5) {
   p5 = _p5;
 
   p5.setup = function() {
-    var canvas = p5.createCanvas(800, 500);
+    var canvas = p5.createCanvas(800, 600);
     canvas.parent("p5Canvas");
-    values = new Array(p5.width);
+    values = new Array(Math.floor(p5.width / w));
     for (let i = 0; i < values.length; i++) {
       values[i] = p5.random(p5.height);
     }
 
-    // Sort
+    p5.frameRate(20);
   };
 
   p5.draw = function() {
     p5.background(0);
 
     if (i < values.length) {
-      for (let j = 0; j < values.length - i - 1; j++) {
-        var a = values[j];
-        var b = values[j + 1];
-        if (a > b) {
-          swap(values, j, j + 1);
+      j++;
+      if (j >= values.length - i - 1) {
+        j = 0;
+        i++;
+      }
+      var a = values[j];
+      var b = values[j + 1];
+      if (a > b) {
+        swap(values, j, j + 1);
+      }
+
+      for (let i = 0; i < values.length; i++) {
+        p5.stroke(200);
+        if (i - 1 === j) {
+          p5.fill(255, 0, 0);
+        } else {
+          p5.fill(255);
         }
+        p5.rect(i * w, p5.height - values[i], w, values[i]);
       }
     } else {
-      p5.noLoop();
-    }
-    i++;
-
-    for (let i = 0; i < values.length; i++) {
-      p5.stroke(255);
-      p5.line(i, p5.height, i, p5.height - values[i]);
+      for (let i = 0; i < values.length; i++) {
+        p5.stroke(200);
+        if (i < z) {
+          p5.fill(0, 255, 0);
+        } else {
+          p5.fill(255);
+        }
+        p5.rect(i * w, p5.height - values[i], w, values[i]);
+      }
+      z++;
+      if (z === values.length) {
+        p5.noLoop();
+      }
     }
   };
 
--- src/assets/js/quicksort.js
+++ src/assets/js/quicksort.js
@@ -3,28 +3,88 @@ let p5;
 export function main(_p5) {
   p5 = _p5;
 
-  var speed = 2;
-  var posX = 0;
+  let values;
+  let w = 10;
+
+  let states;
 
   p5.setup = function() {
-    var canvas = p5.createCanvas(500, 500);
+    var canvas = p5.createCanvas(800, 600);
     canvas.parent("p5Canvas");
+
+    values = new Array(Math.floor(p5.width / w));
+    states = new Array(Math.floor(p5.width / w));
+    for (let i = 0; i < values.length; i++) {
+      values[i] = p5.random(p5.height);
+      states[i] = -1;
+    }
+    quickSort(values, 0, values.length - 1);
   };
 
   p5.draw = function() {
     p5.background(0);
-    const degree = p5.frameCount * 3;
-    const y = p5.sin(p5.radians(degree)) * 50;
 
-    p5.push();
-    p5.translate(0, p5.height / 2);
-    p5.ellipse(posX, y, 50, 50);
-    p5.pop();
-
-    posX += speed;
-
-    if (posX > p5.width || posX < 0) {
-      speed *= -1;
+    for (let i = 0; i < values.length; i++) {
+      p5.noStroke();
+      if (states[i] == 0) {
+        p5.fill("#E0777D");
+      } else if (states[i] == 1) {
+        p5.fill("#D6FFB7");
+      } else {
+        p5.fill(255);
+      }
+      p5.rect(i * w, p5.height - values[i], w, values[i]);
     }
   };
+
+  async function quickSort(arr, start, end) {
+    if (start >= end) {
+      return;
+    }
+    let index = await partition(arr, start, end);
+    states[index] = -1;
+
+    await Promise.all([
+      quickSort(arr, start, index - 1),
+      quickSort(arr, index + 1, end)
+    ]);
+  }
+
+  async function partition(arr, start, end) {
+    for (let i = start; i < end; i++) {
+      states[i] = 1;
+    }
+
+    let pivotValue = arr[end];
+    let pivotIndex = start;
+    states[pivotIndex] = 0;
+    for (let i = start; i < end; i++) {
+      if (arr[i] < pivotValue) {
+        await swap(arr, i, pivotIndex);
+        states[pivotIndex] = -1;
+        pivotIndex++;
+        states[pivotIndex] = 0;
+      }
+    }
+    await swap(arr, pivotIndex, end);
+
+    for (let i = start; i < end; i++) {
+      if (i != pivotIndex) {
+        states[i] = -1;
+      }
+    }
+
+    return pivotIndex;
+  }
+
+  async function swap(arr, a, b) {
+    await sleep(50);
+    let temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
+  }
+
+  function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
 }
